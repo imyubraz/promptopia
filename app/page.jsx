dc5b2701b92a029b6895249753da1d36dfea9acf
Yubraj <imyubraz@gmail.com>
2023-05-18 10:34:54 +0545

    Basic required components created...

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,12 @@
 // import React from 'react'
     // No longer need to import react in Next.js 13
 
+// import Feed from './../components/Feed';
+    // traditional import in react
+
+import Feed from '@/components/Feed';
+    // importing components in Next.js (@/ => root dir as defined in jsconfig.json)
+
 const Home = () => {
   return (
     <div className="wrapper">
@@ -14,6 +20,7 @@ const Home = () => {
                 Promptopia is an open-source A1 prompting tool for modern world to discover, create and share creative prompts.
             </p>
             {/* feed */}
+            <Feed />
         </section>
     </div>
   )
