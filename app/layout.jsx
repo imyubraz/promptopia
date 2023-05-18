874b5fe606f4cb84f87d4071d36821ca8434c700
Yubraj <imyubraz@gmail.com>
2023-05-18 10:23:22 +0545

    Heading and intro added in home page...

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,7 +11,10 @@ export default function RootLayout({ children }) {
  return (
     <html lang="en">
       <body>
-        <main>
+        <div className="main">
+          <div className="gradient"></div>
+        </div>
+        <main className="app">
         {children}
         </main>
       </body>
--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,19 @@
 
 const Home = () => {
   return (
-    <div>Hello World</div>
+    <div className="wrapper">
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">
+                Discover & Share
+                <br className="max-md:hidden" />
+                <span className="orange_gradient text-center">AI-Powered Prompt</span>
+            </h1>
+            <p className="desc text-center">
+                Promptopia is an open-source A1 prompting tool for modern world to discover, create and share creative prompts.
+            </p>
+            {/* feed */}
+        </section>
+    </div>
   )
 }
 
